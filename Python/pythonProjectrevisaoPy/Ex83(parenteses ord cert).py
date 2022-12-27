5834eeee3c38693e37d17e928f0bbe83ffdf331a
Fernando Alves de Paula <feralveswatts@gmail.com>
2022-12-27 20:22:31 -0300

    Update Ex83(parenteses ord cert).py

--- Python/pythonProjectrevisaoPy/Ex83(parenteses ord cert).py
+++ Python/pythonProjectrevisaoPy/Ex83(parenteses ord cert).py
@@ -1,13 +1,12 @@
 pilha = []
-frase = str(input('Digite alguma expressão: '))
-for i in frase:
-    if i == '(':
-        pilha.append(i)
-    elif i == ')':
+expre = str(input('Digite alguma expressão: '))
+for l in expre:
+    if l == '(':
+        pilha.append('(')
+    elif l == ')':
         if len(pilha) > 0:
             pilha.pop()
-            break
 if len(pilha) == 0:
-    print('Sua expressão é válida!')
+    print('Sua expressão é válida!!')
 else:
     print('Sua expressão não é válida...')
