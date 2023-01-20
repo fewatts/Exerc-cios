0bf8cf1849b3f4448431c4c1ccc1de3ac5dadd9a
Fernando Alves de Paula <feralveswatts@gmail.com>
2023-01-20 15:47:21 -0300

    update ex

--- OOP/Teste_conta.py
+++ OOP/Teste_conta.py
@@ -16,14 +16,36 @@ class Conta:
         conta['Saldo'] += valor
 
 
+    def get_titular(conta):
+        return conta['Titular']
+
+
+def títulos(msg):
+    tam = len(msg) + 10
+    print('~' * tam)
+    print(f'     {msg}')
+    print('¬' * tam)
+
+
+títulos('BANCO WATTs')
 contas = []
 qnt = int(input('Quantas contas deseja criar? '))
 for i in range(0, qnt):
-    n = str(input(f'Número da {i + 1}º conta: '))
-    t = str(input('Titular: '))
-    s = float(input('Saldo: '))
-    l = float(input('Limite: '))
+    while True:
+        n = str(input(f'Número da {i + 1}º conta: '))
+        t = str(input('Titular: '))
+        s = float(input('Saldo: '))
+        l = float(input('Limite: '))
+        resp = str(input('Confirma os dados? [S/N] ')).strip().upper()[0]
+        if resp in 'S':
+            break
     conta = Conta.cria_conta(n, t, s, l)
     contas.append(conta)
     print()
-print(contas)
+count = 1
+for c in contas:
+    títulos(f'Banco WATTs Conta Nº{count}')
+    for k, v in c.items():
+        print(f'{k:<16}{v:>12}')
+    print()
+    count += 1
