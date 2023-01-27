485aabde39752be1e0f7660a66f7acbf6af8570b
Fernando Alves de Paula <feralveswatts@gmail.com>
2023-01-27 08:24:47 -0300

    update ex

--- Python/pythonProjectrevisaoPy/pass.py
+++ Python/pythonProjectrevisaoPy/pass.py
@@ -6,12 +6,11 @@ letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
           'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
           'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
           'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-          'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8',
-          '9', '0', '-', '!', '?', '$', '@', '#', '&', '*']
+          'Y', 'Z']
 while acho != senha:
     acho = ''
     for letra in senha:
-        acholetra = letras[randint(0, 69)]
+        acholetra = letras[randint(0, 52)]
         acho = str(acholetra) + str(acho)
     print(acho)
 print(f'A senha encontrada foi: {acho}')
